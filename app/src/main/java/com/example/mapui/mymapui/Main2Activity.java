3ee9e43165b1fca5dd52c3dd6e4b605b6d2eb5c0
yujin <havt5108@outlook.com>
2017-06-30 18:15:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mapui/mymapui/Main2Activity.java
@@ -0,0 +1,319 @@
+package com.example.mapui.mymapui;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.TabHost;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
+
+import java.util.List;
+
+
+public class Main2Activity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+//    private MapView mMapView;
+//    private boolean isFirstLocation = true;
+//    public LocationClient mLocationClient = null;
+//    public BDLocationListener myListener = new MyLocationListener();
+//    private BaiduMap mBaiduMap;
+//
+//    private double lat;
+//    private double lon;
+
+
+    MapView mMapView;
+    BaiduMap baiduMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        baiduMap = mMapView.getMap();
+        baiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        mMapView = (MapView) findViewById(R.id.bmapView);
+//        mBaiduMap = mMapView.getMap();
+//        mLocationClient = new LocationClient(getApplicationContext());
+//        mLocationClient.registerLocationListener(myListener);
+//        initLocation();
+//        //开始定位
+//        mLocationClient.start();
+
+
+//
+//        findViewById(R.id.nav_camera).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(Main2Activity.this,tets.class));
+//            }
+//        });
+
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+
+            startActivity(new Intent(Main2Activity.this,xml_jkda.class));
+
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+            startActivity(new Intent(Main2Activity.this,xml_jjcs.class));
+
+        } else if (id == R.id.nav_slideshow) {
+            startActivity(new Intent(Main2Activity.this,xml_jjlxr.class));
+
+        } else if (id == R.id.nav_manage) {
+            startActivity(new Intent(Main2Activity.this,SettingsActivity.class));
+
+        } else if (id == R.id.nav_share) {
+
+
+        } else if (id == R.id.nav_send) {
+            startActivity(new Intent(Main2Activity.this,xml_fkbug.class));
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+//        mMapView.onDestroy();
+//    }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+//        mMapView.onResume();
+//    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+//        mMapView.onPause();
+//    }
+//private void setMarker() {
+//    Log.v("pcw","setMarker : lat : "+ lat+" lon : " + lon);
+//    //定义Maker坐标点
+//    LatLng point = new LatLng(lat, lon);
+//    //构建Marker图标
+//    BitmapDescriptor bitmap = BitmapDescriptorFactory
+//            .fromResource(R.drawable.icon_gcoding);
+//    //构建MarkerOption，用于在地图上添加Marker
+//    OverlayOptions option = new MarkerOptions()
+//            .position(point)
+//            .icon(bitmap);
+//    //在地图上添加Marker，并显示
+//    mBaiduMap.addOverlay(option);
+//}
+//    private void setUserMapCenter() {
+//        Log.v("pcw","setUserMapCenter : lat : "+ lat+" lon : " + lon);
+//        LatLng cenpt = new LatLng(lat,lon);
+//        //定义地图状态
+//        MapStatus mMapStatus = new MapStatus.Builder()
+//                .target(cenpt)
+//                .zoom(18)
+//                .build();
+//        //定义MapStatusUpdate对象，以便描述地图状态将要发生的变化
+//        MapStatusUpdate mMapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mMapStatus);
+//        //改变地图状态
+//        mBaiduMap.setMapStatus(mMapStatusUpdate);
+//
+//    }
+//
+//    private void initLocation() {
+//        LocationClientOption option = new LocationClientOption();
+//        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+//        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+//        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+//        int span = 1000;
+//        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+//        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+//        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+//        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+//        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+//        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+//        option.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+//        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+//        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
+//        mLocationClient.setLocOption(option);
+//    }
+//
+//    public class MyLocationListener implements BDLocationListener {
+//
+//        @Override
+//        public void onReceiveLocation(BDLocation location) {
+//            //Receive Location
+//            StringBuffer sb = new StringBuffer(256);
+//            sb.append("time : ");
+//            sb.append(location.getTime());
+//            sb.append("\nerror code : ");
+//            sb.append(location.getLocType());
+//            sb.append("\nlatitude : ");
+//            sb.append(location.getLatitude());
+//            sb.append("\nlontitude : ");
+//            sb.append(location.getLongitude());
+//            sb.append("\nradius : ");
+//            sb.append(location.getRadius());
+//            lat = location.getLatitude();
+//            lon = location.getLongitude();
+//            if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+//                sb.append("\nspeed : ");
+//                sb.append(location.getSpeed());// 单位：公里每小时
+//                sb.append("\nsatellite : ");
+//                sb.append(location.getSatelliteNumber());
+//                sb.append("\nheight : ");
+//                sb.append(location.getAltitude());// 单位：米
+//                sb.append("\ndirection : ");
+//                sb.append(location.getDirection());// 单位度
+//                sb.append("\naddr : ");
+//                sb.append(location.getAddrStr());
+//                sb.append("\ndescribe : ");
+//                sb.append("gps定位成功");
+//
+//            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+//                sb.append("\naddr : ");
+//                sb.append(location.getAddrStr());
+//                //运营商信息
+//                sb.append("\noperationers : ");
+//                sb.append(location.getOperators());
+//                sb.append("\ndescribe : ");
+//                sb.append("网络定位成功");
+//            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+//                sb.append("\ndescribe : ");
+//                sb.append("离线定位成功，离线定位结果也是有效的");
+//            } else if (location.getLocType() == BDLocation.TypeServerError) {
+//                sb.append("\ndescribe : ");
+//                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+//            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+//                sb.append("\ndescribe : ");
+//                sb.append("网络不同导致定位失败，请检查网络是否通畅");
+//            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+//                sb.append("\ndescribe : ");
+//                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+//            }
+//            sb.append("\nlocationdescribe : ");
+//            sb.append(location.getLocationDescribe());// 位置语义化信息
+//            List<Poi> list = location.getPoiList();// POI数据
+//            if (list != null) {
+//                sb.append("\npoilist size = : ");
+//                sb.append(list.size());
+//                for (Poi p : list) {sb.append("\npoi= : ");
+//                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+//                }
+//            }
+//            lat = location.getLatitude();
+//            lon = location.getLongitude();
+//            if(isFirstLocation){
+//                isFirstLocation = false;
+//                setMarker();
+//                setUserMapCenter();
+//            }
+//            Log.v("pcw", "lat : " + lat + " lon : " + lon);
+//            Log.i("BaiduLocationApiDem", sb.toString());
+//        }
+//
+//        @Override
+//        public void onConnectHotSpotMessage(String s, int i) {
+//
+//        }
+
+
+    //}
+
+}
---
+++ app/src/main/java/com/example/mapui/mymapui/xml_jjcs.java
@@ -0,0 +1,54 @@
+package com.example.mapui.mymapui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.model.LatLng;
+
+public class xml_jjcs extends AppCompatActivity {
+//    MapView mMapView;
+//    BaiduMap baiduMap;
+    MarkerOptions markerOptions;
+    Marker marker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_xml_jjcs);
+//        mMapView = (MapView) findViewById(R.id.bmapView);
+////        mBaiduMap = mMapView.getMap();
+////
+//////普通地图
+////        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+//        baiduMap = mMapView.getMap();
+//
+
+    }
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+//        mMapView.onDestroy();
+//    }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+//        mMapView.onResume();
+//    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+//        mMapView.onPause();
+//    }
+
+}
